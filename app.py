71f405dd9b198daf76b2e957520b1ad86dc4b005
WilliamUntung <tungstor1@gmail.com>
2026-02-27 09:30:54 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import joblib
+import numpy as np
+import pandas as pd
+
+
+# Load preprocess and model from MLflow
+# Load preprocessor
+scaler = joblib.load("artifacts/preprocessor.pkl")
+model = joblib.load("artifacts/model.pkl")
+
+def main():
+    st.title('Machine Learning Iris Prediction Model Deployment')
+
+    # Add user input components for 5 features
+    sepal_length = st.number_input('input nilai sepal_length', min_value=0.0, max_value=10.0, value=0.1)
+    sepal_width = st.number_input('sepal_width', min_value=0.0, max_value=10.0, value=0.1)
+    patal_length = st.number_input('patal_length', min_value=0.0, max_value=10.0, value=0.1)
+    patal_width = st.number_input('patal_width', min_value=0.0, max_value=10.0, value=0.1)
+    
+    if st.button('Make Prediction'):
+        features = [sepal_length,sepal_width,patal_length,patal_width]
+        result = make_prediction(features)
+        st.success(f'The prediction is: {result}')
+
+def make_prediction(features):
+    # Use the loaded model to make predictions
+    # Replace this with the actual code for your model
+    input_array = np.array(features).reshape(1, -1)
+    X_scaled = scaler.transform(input_array)
+    prediction = model.predict(X_scaled)
+    return prediction[0]
+
+if __name__ == '__main__':
+    main()
+
